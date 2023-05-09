0034166392a22e90777f9c80d60090eb0ec1cbd0
Ulrick Sanchez <ulricksanchez@gmail.com>
2023-05-09 12:00:50 +0800

    Initial Commit

--- 07-events/05event-keycode/script.js
+++ 07-events/05event-keycode/script.js
@@ -1,1 +1,12 @@
 // https://www.toptal.com/developers/keycode
+
+const keyInput = document.getElementById('key1');
+const keyInput2 = document.getElementById('key2');
+const keyInput3 = document.getElementById('key3');
+
+const onKeyDown = (e) => {
+  console.log('e.key');
+  document.querySelector('s') = e.key;
+};
+
+keyInput.addEventListener('keypress', onKeyDown);
--- 08-shopping-list/00-starter-files/shopping-list/script.js
+++ 08-shopping-list/00-starter-files/shopping-list/script.js
@@ -1,12 +1,56 @@
-function addItem() {
-  const input = document.getElementById('item-input').value;
+// function addItem() {
+//   const input = document.getElementById('item-input').value;
 
-  const newItem = document.createElement('li');
-  newItem.innerHTML = `${input}   <button class="remove-item btn-link text-red">
-  <i class="fa-solid fa-xmark"></i>
-</button>`;
-  // newItem.textContent = input;
+//   const newItem = document.createElement('li');
+//   newItem.innerHTML = `${input}   <button class="remove-item btn-link text-red">
+//   <i class="fa-solid fa-xmark"></i>
+// </button>`;
+//   // newItem.textContent = input;
 
-  const list = document.getElementById('item-list');
-  list.appendChild(newItem);
+//   const list = document.getElementById('item-list');
+//   list.appendChild(newItem);
+// }
+
+const clearBtn = document.querySelector('#clear');
+
+// function onClear() {
+//   alert('Clear Items from function');
+// }
+
+//################################################
+//clearing them all
+function onClear() {
+  const itemList = document.querySelector('ul');
+  itemList.innerHTML = '';
 }
+//################################################
+
+function onClear() {
+  const itemList = document.querySelector('ul');
+  const items = document.querySelectorAll('li');
+
+  //forEach
+  items.forEach((item) => item.remove());
+}
+
+//Javascript Event Listener
+// clearBtn.onlick = function () {
+//   alert('Clearn Items');
+// };
+
+//addEventListener()
+// clearBtn.addEventListener('click', function () {
+//   alert('Clear Items');
+// });
+
+//arrow function to shorten
+// clearBtn.addEventListener('click', () => alert('Clear Items'));
+// clearBtn.addEventListener('click', () => console.log('Clear Items'));
+
+//named function
+clearBtn.addEventListener('click', onClear);
+
+//removeEventListener()
+setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
+//after 5 seconds, the event listener is now removed
+//it will not work anymore
