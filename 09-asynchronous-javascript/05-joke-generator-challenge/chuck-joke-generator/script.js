f6db9e82db7fc70b674585b6529b15a329e10f81
Ulrick Sanchez <ulricksanchez@gmail.com>
2023-05-10 14:11:39 +0800

    Initial Commit

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -1,30 +1,3 @@
-// const jokeBtn = document.querySelector('#joke-btn');
-// const jokeText = document.querySelector('#joke');
-
-// jokeBtn.addEventListener('click', generateJoke);
-
-// function generateJoke() {
-//   const xhr = new XMLHttpRequest();
-//   const url = 'https://api.chucknorris.io/jokes/random';
-
-//   xhr.open('GET', url, true);
-
-//   xhr.onload = function () {
-//     if (this.status === 200) {
-//       const jokeData = JSON.parse(this.responseText);
-//       jokeText.innerHTML = jokeData.value;
-//     } else {
-//       jokeText.innerHTML = 'Oops! Something went wrong.';
-//     }
-//   };
-
-//   xhr.onerror = function () {
-//     jokeText.innerHTML = 'Oops! Something went wrong.';
-//   };
-
-//   xhr.send();
-// }
-
 const button = document.querySelector('#joke-btn');
 const joke = document.querySelector('#joke');
 
