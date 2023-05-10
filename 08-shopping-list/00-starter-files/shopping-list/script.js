808e918d048c16dd4daec290066c923e50dbc8f8
Ulrick Sanchez <ulricksanchez@gmail.com>
2023-05-10 14:03:39 +0800

    Initial Commit

--- 08-shopping-list/00-starter-files/shopping-list/script.js
+++ 08-shopping-list/00-starter-files/shopping-list/script.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-// function addItem() {
-//   const input = document.getElementById('item-input').value;
-
-//   const newItem = document.createElement('li');
-//   newItem.innerHTML = `${input}   <button class="remove-item btn-link text-red">
-//   <i class="fa-solid fa-xmark"></i>
-// </button>`;
-//   // newItem.textContent = input;
-
-//   const list = document.getElementById('item-list');
-//   list.appendChild(newItem);
-// }
-
-const clearBtn = document.querySelector('#clear');
-
-// function onClear() {
-//   alert('Clear Items from function');
-// }
-
-//################################################
-//clearing them all
-function onClear() {
-  const itemList = document.querySelector('ul');
-  itemList.innerHTML = '';
-}
-//################################################
-
-function onClear() {
-  const itemList = document.querySelector('ul');
-  const items = document.querySelectorAll('li');
-
-  //forEach
-  items.forEach((item) => item.remove());
-}
-
-//Javascript Event Listener
-// clearBtn.onlick = function () {
-//   alert('Clearn Items');
-// };
-
-//addEventListener()
-// clearBtn.addEventListener('click', function () {
-//   alert('Clear Items');
-// });
-
-//arrow function to shorten
-// clearBtn.addEventListener('click', () => alert('Clear Items'));
-// clearBtn.addEventListener('click', () => console.log('Clear Items'));
-
-//named function
-clearBtn.addEventListener('click', onClear);
-
-//removeEventListener()
-setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
-//after 5 seconds, the event listener is now removed
-//it will not work anymore
-=======
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
@@ -245,4 +187,3 @@ function init() {
 }
 
 init();
->>>>>>> upstream/master
--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -1,26 +1,49 @@
-const jokeBtn = document.querySelector('#joke-btn');
-const jokeText = document.querySelector('#joke');
+// const jokeBtn = document.querySelector('#joke-btn');
+// const jokeText = document.querySelector('#joke');
 
-jokeBtn.addEventListener('click', generateJoke);
+// jokeBtn.addEventListener('click', generateJoke);
+
+// function generateJoke() {
+//   const xhr = new XMLHttpRequest();
+//   const url = 'https://api.chucknorris.io/jokes/random';
+
+//   xhr.open('GET', url, true);
+
+//   xhr.onload = function () {
+//     if (this.status === 200) {
+//       const jokeData = JSON.parse(this.responseText);
+//       jokeText.innerHTML = jokeData.value;
+//     } else {
+//       jokeText.innerHTML = 'Oops! Something went wrong.';
+//     }
+//   };
+
+//   xhr.onerror = function () {
+//     jokeText.innerHTML = 'Oops! Something went wrong.';
+//   };
+
+//   xhr.send();
+// }
+
+const button = document.querySelector('#joke-btn');
+const joke = document.querySelector('#joke');
+
+button.addEventListener('click', generateJoke);
 
 function generateJoke() {
   const xhr = new XMLHttpRequest();
-  const url = 'https://api.chucknorris.io/jokes/random';
 
-  xhr.open('GET', url, true);
-
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const jokeData = JSON.parse(this.responseText);
-      jokeText.innerHTML = jokeData.value;
+  xhr.open('Get', 'https://api.chucknorris.io/jokes/random');
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      joke.innerHTML = data.value;
     } else {
-      jokeText.innerHTML = 'Oops! Something went wrong.';
+      joke.innerHTML = 'Error loading joke';
     }
-  };
-
-  xhr.onerror = function () {
-    jokeText.innerHTML = 'Oops! Something went wrong.';
   };
 
   xhr.send();
 }
+
+document.addEventListener('DOMContentLoaded', generateJoke);
